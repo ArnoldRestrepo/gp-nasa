67c96736cb03021c985c2b8fcc8fdd22aeb01dc8
Arnold Restrepo Hernandez <arnold.restrepohernandez@gmail.com>
2022-03-22 00:22:46 -0500

    Initial Commit

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,11 @@
+export const Login = () => { 
+    return (
+        <div>
+            <h1>Login</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas, quibusdam.
+            </p>
+        </div>
+    )
+}
